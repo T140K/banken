5d1eb50ac12d1965f35ef5f1cdd188ccbf4fe205
T140K <darek1430143@gmail.com>
2023-01-04 11:48:54 +0100

    jag har börjat koda och har skapat en array och class med några hårdkodade användare och även skapat ett sätt att skapa nya via en constructor. Jag har också börjat jobba på att kunna logga in. Allt är i en while loop och enda sättet att råka stänga applikationen borde vara att skriva 3 i det första menyn

--- Banken/Program.cs
+++ Banken/Program.cs
@@ -1,5 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Reflection.Metadata;
+using System.Transactions;
 
 //måste använda array för store user info
 //helst constructor med methods för de bilr enklare
@@ -29,6 +31,57 @@ namespace Banken
             //else if (choice == 3) {
             //break; }
 
+            // skapar en array som är baserad på constructorn jag har
+            User[] users = new User[3];
+            users[0] = new User("admin", "admin", 50000);
+            users[1] = new User("user1", "password1", 350.5);
+            users[2] = new User("user1", "password1", 0);
+
+            //while loop för att appen inte ska stängas tills man skriver in "3"
+            while (true)
+            {
+                Console.WriteLine("Welcome to the bank app, what would you like to do?");
+                Console.WriteLine("1. Login");
+                Console.WriteLine("2. Register");
+                Console.WriteLine("3. exit");
+
+                string choice = Console.ReadLine();
+
+                if (choice == "1")
+                {
+                    Console.Write("Enter your username: ");
+                    string inputUser = Console.ReadLine();
+                    Console.Write("Enter your password: ");
+                    string inputPass = Console.ReadLine();
+                    
+                   
+                }
+                if (choice == "3")
+                {
+                    break;
+                }
+            }
+        }
+
+        public class User
+        {
+            private string _username;
+            private string _password;
+            private double _balance;
+
+            //constructor för att kunna skapa nya element till arrayen senare
+            public User(string username, string password, double balance)
+            {
+                this._username = username;
+                this._password = password;
+                this._balance = balance;
+            }
+            
+            //metod som kollar double i arrayen
+            public string showBalance()
+            {
+                return "You have: " + this._balance + "$";
+            }
         }
     }
 }
