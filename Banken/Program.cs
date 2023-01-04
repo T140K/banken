c463a9645484f905166a67b6f34c62e40de045f5
T140K <darek1430143@gmail.com>
2023-01-04 15:24:02 +0100

    added menus for login, and exit, now cna check, deposit, withdraw and transfer money and logout from the account

--- Banken/Program.cs
+++ Banken/Program.cs
@@ -38,13 +38,10 @@ namespace Banken
             users[1] = new User("user1", "123", 350.5);
             users[2] = new User("user2", "1234", 0);
 
-            Console.WriteLine(users[0].showBalance());
-            Console.WriteLine(users[0].getUsername());
-
             //while loop för att appen inte ska stängas tills man skriver in "3"
             while (true)
             {
-                Console.WriteLine("Welcome to the bank app, what would you like to do?");
+                Console.WriteLine("Welcome, what would you like to do?");
                 Console.WriteLine("1. Login");
                 Console.WriteLine("2. Register");
                 Console.WriteLine("3. exit");
@@ -53,11 +50,14 @@ namespace Banken
 
                 if (choice == "1")
                 {
+                    Console.Clear();
                     Console.Write("Enter your username: ");
-                    string inputUser = Console.ReadLine();
+                    string? inputUser = Console.ReadLine();
                     Console.Write("Enter your password: ");
-                    string inputPass = Console.ReadLine();
+                    string? inputPass = Console.ReadLine();
 
+                    
+                    credentialsValidation(users, inputUser, inputPass);
                     /*bool login = false; //dethär var rarted, borde göra det som en function som jag kommenterade om nere
                     User loggedInUser;
                     foreach (User user in users)
@@ -94,7 +94,7 @@ namespace Banken
                         }
                         if (subOption == "4")
                         {
-
+                            break;
                         }
                     }
                     else
@@ -104,7 +104,109 @@ namespace Banken
                 }
                 if (choice == "3")
                 {
+                    Console.Clear();
                     break;
+                }
+            }
+            //denna funktion kollar om de info man gav ovan är korrekt med inputuser och inputpass som checkas 
+            //tillasmmans med det som finns i array"databasen" då om det finns sådan kombination kommer man
+            //logga in med den och denna kombinationen kommer spara som user och skickas vidare till userloggedin functionen
+            void credentialsValidation(User[] allUsers, string inputUser, string inputPass)
+            {
+                foreach (User user in allUsers)
+                {
+                    if (user.Username == inputUser && user.Password == inputPass)
+                    {
+                        userLoggedIn(user);
+                        break;
+                    }
+                }
+            }
+            //i denna metoden får man validerad kombination av en login som skickas in i denna parameter och blir till loggedinuser 
+            //man får även access till andra arrayer så att man kan transfer pengar till andra konto, allt är i en while
+            //loop så att man inte stänger ner applikationen när man loggar ut för att byta användare
+            void userLoggedIn (User loggedInUser) 
+            {
+                while (true)
+                {
+                    Console.Clear();
+                    Console.WriteLine($"Hello {loggedInUser.Username}! What would you like to do?\n");
+                    Console.WriteLine("1. Check your balance");
+                    Console.WriteLine("2. Deposit money");
+                    Console.WriteLine("3. Withdraw money");
+                    Console.WriteLine("4. Transfer money");
+                    Console.WriteLine("5. Logout");
+
+                    string? option = Console.ReadLine();
+                    if (option == "1")
+                    {
+                        Console.Clear();
+                        Console.WriteLine("Your balance is " + loggedInUser.Balance);
+                        Console.ReadLine();
+                    }
+                    if (option == "2")
+                    {
+                        //+= gör att loggedinuser.Balance först adderas och sedan uppdateras till nya värdet
+                        Console.Clear();
+                        Console.Write("You have " + loggedInUser.Balance + "$, how much would you like to deposit: ");
+                        double ammount = double.Parse(Console.ReadLine());
+                        loggedInUser.Balance += ammount;
+                        Console.Clear();
+                        Console.WriteLine("You now have " + loggedInUser.Balance + "$");
+                        Console.ReadLine();
+                    }
+                    if (option == "3")
+                    {
+                        //+= gör att loggedinuser.Balance först subtraheras och sedan uppdateras till nya värdet
+                        Console.Clear();
+                        Console.Write("You have " + loggedInUser.Balance + "$, how much would you like to withdraw: ");
+                        double ammount = double.Parse(Console.ReadLine());
+                        loggedInUser.Balance -= ammount;
+                        Console.Clear();
+                        Console.WriteLine("You now have " + loggedInUser.Balance + "$");
+                        Console.ReadLine();
+                    }
+                    if (option == "4")
+                    {
+                        Console.Clear();
+                        Console.WriteLine("What account do you want to transfer money to?");
+                        string account = Console.ReadLine();
+                        Console.Clear();
+                        Console.WriteLine("How much money would you like to transfer?");
+                        double ammount = double.Parse(Console.ReadLine());
+                        loggedInUser.Balance -= ammount;
+                        //an kollar Users array och skapar newUser variabel som kommer sättas till den user man hittar i 
+                        //account == newUser.Username, man söker genom hela listen och gemför den med inputen
+                        foreach (User newUser in users)
+                        {
+                            if (account == newUser.Username) 
+                            {
+                                if (ammount > loggedInUser.Balance)
+                                {
+                                    //om man inte har pengar i kontot kommer denna meddelande visas
+                                    Console.WriteLine("You dont have enought money");
+                                    Console.ReadLine();
+                                }
+                                else
+                                {
+                                    //annars är allt bra och det kommer gå igenom så att newUser balance adderas och 
+                                    //uppdateras med summan gav 
+                                    newUser.Balance += ammount;
+                                }
+                            }
+                            else
+                            {
+                                //om man inte lyvkas kommer man gå tillbaka
+                                Console.WriteLine("Something went wrong, try again.");
+                            }
+                        }
+
+                    }
+                    if (option == "5")
+                    {
+                        Console.Clear();
+                        break;
+                    }
                 }
             }
         }
@@ -136,15 +238,7 @@ namespace Banken
             }
             
             //metod som kollar double i arrayen
-            public string showBalance()
-            {
-                return "You have: " + Balance + "$";
-            }
-
-            public string getUsername()
-            {
-                return Username;
-            }
+            //metoderna var retardeD?
         }
         //varje user ska ha flera kontos, minst 2
         //nested array
