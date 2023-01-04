81bc744d4d819eaed2c6d575ac03d3589438da57
T140K <darek1430143@gmail.com>
2023-01-04 11:03:20 +0100

    first commit

---
+++ Banken/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+
+//måste använda array för store user info
+//helst constructor med methods för de bilr enklare
+//double borde fungera för hålla balance?
+//måste ha flera bools o loops
+//minst tre funktioner/metoder
+//
+// LÖPANDE SPARING TILL GIT UNDER ARBETSPROCESS
+//
+//Kommentera det som inte är självklart, alla metoder
+
+//läs VG kriterier när jage klar!!!
+
+namespace Banken
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //while loop för hela menyn, array som e med constructor
+
+            //while {
+            //det ska va 1.Login 2.Register 3.Exit
+            //string choice = Console.ReadLine();
+            //if (choice == 1) Login
+            //else if (choice == 2) Register
+            //else if (choice == 3) {
+            //break; }
+
+        }
+    }
+}
